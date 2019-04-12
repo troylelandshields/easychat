7062ff0bf7afcd440114ca30d33962b964cc2b90
troylelandshields <troy.leland.shields@gmail.com>
2019-04-11 18:45:17 -0600

    let's have JoinChatRoom return an error and also added godoc

--- easychat.go
+++ easychat.go
@@ -2,28 +2,32 @@ package easychat
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net"
 	"time"
 )
 
+// ChatClient has functionality for sending and receiving messages from an EasyChatServer
 type ChatClient struct {
 	username         string
 	outgoingMessages *json.Encoder
 	incomingMessages *json.Decoder
 }
 
-func JoinChatRoom(ipAddr string, username string) *ChatClient {
+// JoinChatRoom requires the IP Address of an EasyChatServer and a username to use for connecting.
+// It returns a ChatClient or an error if we can't connect.
+func JoinChatRoom(ipAddr string, username string) (*ChatClient, error) {
 	conn, err := net.Dial("tcp", ipAddr+":1234")
 	if err != nil {
-		log.Fatal("Error connecting to chat server:", err)
+		return nil, errors.New("Error connecting to chat server: " + err.Error())
 	}
 
 	// send connection information
 	enc := json.NewEncoder(conn)
 	err = enc.Encode(username)
 	if err != nil {
-		log.Fatal(err)
+		return nil, errors.New("Error sending username to chatroom: " + err.Error())
 	}
 
 	dec := json.NewDecoder(conn)
@@ -32,15 +36,17 @@ func JoinChatRoom(ipAddr string, username string) *ChatClient {
 		username:         username,
 		outgoingMessages: enc,
 		incomingMessages: dec,
-	}
+	}, nil
 }
 
+// ChatMessage contains a message, the author, and the timestamp of the message.
 type ChatMessage struct {
 	From string
 	Body string
 	Time time.Time
 }
 
+// SendMessage will deliver the messageText in a ChatMessage to the EasyChatServer so it can be sent to everyone in the chatroom
 func (chat *ChatClient) SendMessage(messageText string) {
 	msg := ChatMessage{
 		From: chat.username,
@@ -54,6 +60,7 @@ func (chat *ChatClient) SendMessage(messageText string) {
 	}
 }
 
+// ReceiveMessage will return the next message that has been sent in the chatroom from the EasyChatServer
 func (chat *ChatClient) ReceiveMessage() (ChatMessage, bool) {
 	var incomingMsg ChatMessage
 
