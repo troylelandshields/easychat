582d3afc9e9c9ea1c25fb34de45559789b7be724
troylelandshields <troy.leland.shields@gmail.com>
2019-04-11 19:02:52 -0600

    remove code snippets

--- easychat.go
+++ easychat.go
@@ -17,12 +17,6 @@ type ChatClient struct {
 
 // JoinChatRoom requires the IP Address of an EasyChatServer and a username to use for connecting.
 // It returns a ChatClient or an error if we can't connect.
-//  chatClient, err := JoinChatRoom("128.0.0.1", "ExampleUser")
-//  if err != nil {
-//	   fmt.Println("Error occurred:", err.Error())
-//     return
-//  }
-
 func JoinChatRoom(ipAddr string, username string) (*ChatClient, error) {
 	conn, err := net.Dial("tcp", ipAddr+":1234")
 	if err != nil {
@@ -53,7 +47,6 @@ type ChatMessage struct {
 }
 
 // SendMessage will deliver the messageText in a ChatMessage to the EasyChatServer so it can be sent to everyone in the chatroom
-//  chatClient.SendMessage("Hi! I'm sending you a message")
 func (chat *ChatClient) SendMessage(messageText string) {
 	msg := ChatMessage{
 		From: chat.username,
@@ -68,8 +61,6 @@ func (chat *ChatClient) SendMessage(messageText string) {
 }
 
 // ReceiveMessage will return the next message that has been sent in the chatroom from the EasyChatServer.
-//  msg := chatClient.ReceiveMessage()
-//  fmt.Println(msg.Body)
 func (chat *ChatClient) ReceiveMessage() ChatMessage {
 	var incomingMsg ChatMessage
 
