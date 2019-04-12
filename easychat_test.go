ca721182085b2f83ee26a9280d51b8c017dee4b8
troylelandshields <troy.leland.shields@gmail.com>
2019-04-11 19:01:44 -0600

    add an example

--- easychat_test.go
+++ easychat_test.go
@@ -3,5 +3,14 @@ package easychat
 import "fmt"
 
 func ExampleJoinChatRoom() {
-	fmt.Println("Here")
+	chatClient, err := JoinChatRoom("128.0.0.1", "ExampleUser")
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		return
+	}
+
+	msg := chatClient.ReceiveMessage()
+	fmt.Println("Message from", msg.From, msg.Body)
+
+	chatClient.SendMessage("Hi!")
 }
